1beef6eb52eaa991683e4736efa2dd84ecb25919
Dimitrios Chatzinikolis <dimitrioschatzinikolis@gmail.com>
2023-05-24 00:16:11 -0400

    Updates

--- output/server/nodes/0.js
+++
@@ -1,9 +0,0 @@
-import * as universal from '../entries/pages/_layout.js';
-
-export const index = 0;
-export const component = async () => (await import('../entries/fallbacks/layout.svelte.js')).default;
-export { universal };
-export const universal_id = "src/routes/+layout.js";
-export const imports = ["_app/immutable/nodes/0.7b9ce252.js","_app/immutable/chunks/index.d0c0af47.js"];
-export const stylesheets = [];
-export const fonts = [];
--- output/server/nodes/1.js
+++
@@ -1,7 +0,0 @@
-
-
-export const index = 1;
-export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.3c0c08b4.js","_app/immutable/chunks/index.d0c0af47.js","_app/immutable/chunks/singletons.8f4899e9.js"];
-export const stylesheets = [];
-export const fonts = [];

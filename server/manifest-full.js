bdf7695d24d6767806571023bbb9295d10aea021
Dimitrios Chatzinikolis <dimitrioschatzinikolis@gmail.com>
2023-05-23 23:26:13 -0400

    Updates

--- server/manifest-full.js
+++ server/manifest-full.js
@@ -4,7 +4,7 @@ export const manifest = {
 	assets: new Set([]),
 	mimeTypes: {},
 	_: {
-		client: {"start":"_app/immutable/entry/start.a7afb3f2.js","app":"_app/immutable/entry/app.8718f6d3.js","imports":["_app/immutable/entry/start.a7afb3f2.js","_app/immutable/chunks/index.d0c0af47.js","_app/immutable/chunks/singletons.c64c633b.js","_app/immutable/entry/app.8718f6d3.js","_app/immutable/chunks/index.d0c0af47.js"],"stylesheets":[],"fonts":[]},
+		client: {"start":"_app/immutable/entry/start.8c62b5d1.js","app":"_app/immutable/entry/app.efc4c939.js","imports":["_app/immutable/entry/start.8c62b5d1.js","_app/immutable/chunks/index.d0c0af47.js","_app/immutable/chunks/singletons.2231c7eb.js","_app/immutable/entry/app.efc4c939.js","_app/immutable/chunks/index.d0c0af47.js"],"stylesheets":[],"fonts":[]},
 		nodes: [
 			() => import('./nodes/0.js'),
 			() => import('./nodes/1.js'),
--- server/nodes/1.js
+++ server/nodes/1.js
@@ -2,6 +2,6 @@
 
 export const index = 1;
 export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.1d3eef9d.js","_app/immutable/chunks/index.d0c0af47.js","_app/immutable/chunks/singletons.c64c633b.js"];
+export const imports = ["_app/immutable/nodes/1.7dec0497.js","_app/immutable/chunks/index.d0c0af47.js","_app/immutable/chunks/singletons.2231c7eb.js"];
 export const stylesheets = [];
 export const fonts = [];
